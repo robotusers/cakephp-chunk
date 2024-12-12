6d6fa8be8941cb8c5842bb71933e26be7f459b7e
Robert Pustułka <robert.pustulka@gmail.com>
2024-12-12 09:19:25 +0100

    Setup test env

---
+++ tests/schema.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'authors' => [
+        'columns' => [
+            'id' => [
+                'type' => 'integer',
+            ],
+            'name' => [
+                'type' => 'string',
+                'default' => null,
+            ],
+        ],
+        'constraints' => [
+            'primary' => [
+                'type' => 'primary',
+                'columns' => [
+                    'id',
+                ],
+            ],
+        ],
+    ],
+];
